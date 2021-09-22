cd7614bd0f5d5f957dced8dde5afcd5bf26c7d91
John Kiernan <jkiernan12@yahoo.com>
2021-09-22 17:35:57 -0400

    Add Player class with methods

---
+++ Player.js
@@ -0,0 +1,20 @@
+class Player {
+    constructor(id, token, wins) {
+        this.id = id;
+        this.token = token;
+        this.wins = wins || 0;
+    }
+
+    saveWinsToStorage() {
+        window.localStorage.setItem(`${this.id}`, JSON.stringify(this.wins));
+    }
+
+    retrieveWinsFromStorage() {
+        var retrieved = window.localStorage.getItem(`${this.id}`);
+        this.wins = parseInt(retrieved);
+    }
+
+    addwin() {
+        this.wins++;
+    }
+}
