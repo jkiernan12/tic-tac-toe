1edca0cfa8dfb1691dccd9cced8caa16aa1a8232
John Kiernan <jkiernan12@yahoo.com>
2021-09-29 14:45:10 -0400

    Change hard tabs to soft tabs

--- Player.js
+++ Player.js
@@ -1,23 +1,23 @@
 class Player {
-    constructor(id, token, wins) {
-        this.id = id;
-        this.token = token;
-        this.wins = wins || 0;
-        this.moves = [];
-    }
+  constructor(id, token, wins) {
+    this.id = id;
+    this.token = token;
+    this.wins = wins || 0;
+    this.moves = [];
+  }
 
-    saveWinsToStorage() {
-        window.localStorage.setItem(`${this.id}`, JSON.stringify(this.wins));
-    }
+  saveWinsToStorage() {
+    window.localStorage.setItem(`${this.id}`, JSON.stringify(this.wins));
+  }
 
-    retrieveWinsFromStorage() {
-        if (window.localStorage.getItem(`${this.id}`)) {
-            var retrieved = window.localStorage.getItem(`${this.id}`);
-            this.wins = parseInt(retrieved);
-        }
+  retrieveWinsFromStorage() {
+    if (window.localStorage.getItem(`${this.id}`)) {
+      var retrieved = window.localStorage.getItem(`${this.id}`);
+      this.wins = parseInt(retrieved);
     }
+  }
 
-    addWin() {
-        this.wins++;
-    }
+  addWin() {
+    this.wins++;
+  }
 }
--- main.js
+++ main.js
@@ -14,92 +14,92 @@ var game = new Game();
 
 // Functions
 function makeMove(event) {
-    var moveCoordinates = [];
-    moveCoordinates.push(parseInt(event.target.dataset.x));
-    moveCoordinates.push(parseInt(event.target.dataset.y));
+  var moveCoordinates = [];
+  moveCoordinates.push(parseInt(event.target.dataset.x));
+  moveCoordinates.push(parseInt(event.target.dataset.y));
 
-    if (game.checkMove(moveCoordinates)) {
-        game.addMove(moveCoordinates);
-        renderMove();
-        game.checkGame();
-        game.toggleTurn();
-        renderGame();
-    }
+  if (game.checkMove(moveCoordinates)) {
+    game.addMove(moveCoordinates);
+    renderMove();
+    game.checkGame();
+    game.toggleTurn();
+    renderGame();
+  }
 }
 
 function startGame() {
-    renderMove();
-    renderGame();
+  renderMove();
+  renderGame();
 }
 
 function renderMove() {
-    renderWins();
-    renderBoard("player1");
-    renderBoard("player2");
+  renderWins();
+  renderBoard("player1");
+  renderBoard("player2");
 }
 
 function renderGame() {
-    if (!game.winState) {
-        renderFeedback(`It's ${game[game.currentTurn].token}'s turn!`);
-    } else { 
-        renderWinner();
-    }
+  if (!game.winState) {
+    renderFeedback(`It's ${game[game.currentTurn].token}'s turn!`);
+  } else { 
+    renderWinner();
+  }
 }
 
 function renderWinner() {
-    if (game.winner === "draw") {
-        renderFeedback(`It's a draw!`);
-    } else {
-        animate(trophy, "trophy-animation")
-        renderFeedback(`${game[game.winner].token} won!`);
-    }
-    renderWins();
-    board.classList.add("disable-clicks");
-    setTimeout(resetBoard, 2000);
-    
+  if (game.winner === "draw") {
+    renderFeedback(`It's a draw!`);
+  } else {
+    animate(trophy, "trophy-animation")
+    renderFeedback(`${game[game.winner].token} won!`);
+  }
+  renderWins();
+  board.classList.add("disable-clicks");
+  setTimeout(resetBoard, 2000);
+  
 }
 
 function renderWins() {
-    game.player1.retrieveWinsFromStorage();
-    game.player2.retrieveWinsFromStorage();
-    playerOneWins.innerText = `${game.player1.wins} wins`;
-    playerTwoWins.innerText = `${game.player2.wins} wins`;
+  game.player1.retrieveWinsFromStorage();
+  game.player2.retrieveWinsFromStorage();
+  playerOneWins.innerText = `${game.player1.wins} wins`;
+  playerTwoWins.innerText = `${game.player2.wins} wins`;
 }
 
 function renderBoard(player) {
-    for (var i = 0; i < game[player].moves.length; i++) {
-        var yCoordinate = game[player].moves[i][1];
-        var xCoordinate = game[player].moves[i][0];
-        var currentBox = document.querySelector(`[data-x="${xCoordinate}"][data-y="${yCoordinate}"]`);
-        currentBox.innerText = game[player].token;
-    }
+  for (var i = 0; i < game[player].moves.length; i++) {
+    var yCoordinate = game[player].moves[i][1];
+    var xCoordinate = game[player].moves[i][0];
+    var currentBox = document.querySelector(`[data-x="${xCoordinate}"][data-y="${yCoordinate}"]`);
+    currentBox.innerText = game[player].token;
+  }
 }
 
 function renderFeedback(message) {
-    animate(gameFeedback, "feedback-animation");
-    gameFeedback.innerText = message;
+  animate(gameFeedback, "feedback-animation");
+  gameFeedback.innerText = message;
 }
 
 function resetBoard() {
-    clearBoard();
-    renderFeedback(`It's ${game[game.currentTurn].token}'s turn!`);
-    trophy.classList.remove("trophy-animation");
-    board.classList.remove("disable-clicks");
+  clearBoard();
+  renderFeedback(`It's ${game[game.currentTurn].token}'s turn!`);
+  trophy.classList.remove("trophy-animation");
+  board.classList.remove("disable-clicks");
 }
 
 function clearBoard() {
-    var squares = document.querySelectorAll(".board__square");
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].innerText = "";
-    }
+  var squares = document.querySelectorAll(".board__square");
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].innerText = "";
+  }
 }
 
 function animate (element, className) {
+  window.requestAnimationFrame(function() {
     window.requestAnimationFrame(function() {
-        window.requestAnimationFrame(function() {
-            element.classList.add(className);
-            
-        })
+      element.classList.add(className);
+      
     })
-    element.classList.remove(className);
+  })
+  element.classList.remove(className);
 }
